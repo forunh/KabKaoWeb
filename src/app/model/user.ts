31c22c86c07e7ca404d651dcb8f39838daa76e21
oat9002 <oat9002@hotmail.com>
2017-03-01 19:51:56 +0700

    Validate form

---
+++ src/app/model/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    constructor(
+        public username: string,
+        public firstname: string,
+        public surname: string,
+        public email: string,
+        public address: string,
+    ) {}
+}
--- src/app/sign_up.component.ts
+++ src/app/sign_up.component.ts
@@ -1,10 +1,24 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { User } from './model/user';
 
 @Component({
     selector: 'sign-up',
-    templateUrl: '../html/sign_up.component.html'
+    templateUrl: '../html/sign_up.component.html',
 })
 export class SignUpComponent {
-   signUpTitle = 'Sign Up';
-   @Input() test: String;
+    isValid = false;
+    signUpTitle = 'Sign Up';
+    submitted = false;
+    
+    username: string;
+    password: string;
+    firstname: string;
+    surname: string;
+    email: string;
+    address: string;
+    
+    onSubmit() {
+        this.submitted = true;
+    }
 }
