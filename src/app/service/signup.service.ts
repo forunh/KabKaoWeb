4fbd1e5131c65b7659edf07523e0c52e894b42ab
Pittayarat <pittayarat.sae@gmail.com>
2017-03-03 05:51:03 +0700

    Revert "Now can register to website"
    This reverts commit 42f098294db3e46969aad58fa6f310ba1c5cac54.

--- src/app/service/signup.service.ts
+++ src/app/service/signup.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
+import {Observable} from 'rxjs/Rx';
 import { User } from '../model/user';
 import 'rxjs/add/operator/map';
 
@@ -8,8 +9,10 @@ export class SignUpService {
     private headers = new Headers({'Content-Type': 'application/json'});
     constructor(private http:Http) { }
 
-    public addUser(user: User) {
-        return this.http.post('http://52.187.62.107:10300/authen/sign_up', JSON.stringify(user), {headers: this.headers})
-                .map((res: Response) => res.json());
+    public addUser(user:User){
+        return Observable.forkJoin(
+        this.http.post('http://52.187.62.107:10300/authen/sign_up', JSON.stringify(user), {headers: this.headers})
+            .map((res:Response) => res.json())
+        );
     }
 }
