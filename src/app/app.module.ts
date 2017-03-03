fceb02042a7443731f744b2b5a6ff9c2248440e1
Pittayarat <pittayarat.sae@gmail.com>
2017-03-04 02:31:21 +0700

    change some component to master version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
+import {CollapseModule, ModalModule} from 'ng2-bootstrap';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
 import {AppComponent} from './app.component';
 import {CollapseComponent} from './collapse/collapse.component';
-
-import {CollapseModule, ModalModule} from 'ng2-bootstrap';
 import {OrderComponent} from './order/order.component';
 import {MenubarComponent} from './menubar/menubar.component';
 import {ModalComponent} from './modal/modal.component';
@@ -16,7 +17,6 @@ import {OrderDetailComponent} from './order-detail/order-detail.component'
 import {SignUpComponent} from './signup/signup.component';
 
 import {OrderService} from './service/order.service';
-
 import {SignUpService} from './service/signup.service';
 
 @NgModule({
@@ -37,8 +37,10 @@ import {SignUpService} from './service/signup.service';
         HttpModule,
         CollapseModule.forRoot(),
         ModalModule.forRoot(),
+        AgmCoreModule.forRoot({apiKey: 'AIzaSyAf-hWJtntAe1oGV9UwbVgG-3r25o65p14'}),
+
     ],
-    providers: [SignUpService],
+    providers: [OrderService,SignUpService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
