89be3af21abc923548062766685d5f7e85a54562
Unknown <moobinkung@windowslive.com>
2017-03-05 00:53:26 +0700

    - added getMyUserData function to UserService.

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -8,7 +8,6 @@ export class AuthGuard implements CanActivate{
   constructor(private userService: UserService) {}
 
   canActivate() {
-    console.log('AuthGuard#canActivate called');
     return this.userService.isLogin();
   }
 
