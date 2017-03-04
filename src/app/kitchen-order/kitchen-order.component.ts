d103ae38475653e002944c99d538f6430a5f7631
FatelCypher <kid1412_jj@hotmail.com>
2017-03-05 02:48:44 +0700

    -add ngFor

--- src/app/kitchen-order/kitchen-order.component.ts
+++ src/app/kitchen-order/kitchen-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { KitchenOrder } from '../model/kitchenOrder';
 
 @Component({
   selector: 'app-kitchen-order',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KitchenOrderComponent implements OnInit {
 
-  constructor() { }
+kitchenOrders = [
+    { id: 1000,
+     orderid:1000,
+     menuid:2,
+     toppingid:0,
+     userid:1412,
+     quantity:1,
+     status:"waiting",
+     timestamp:null
+},
+    { id: 1001,
+     orderid:1001,
+     menuid:3,
+     toppingid:1,
+     userid:1412,
+     quantity:2,
+     status:"waiting",
+     timestamp:null
+},
+    {id: 1002,
+     orderid:1002,
+     menuid:1,
+     toppingid:2,
+     userid:1413,
+     quantity:3,
+     status:"waiting",
+     timestamp:null
+}];
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    
   }
+
 
 }
