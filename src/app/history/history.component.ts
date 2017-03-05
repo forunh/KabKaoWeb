c8ab0d046fcc598e3738d9706972746838bb06d0
Pittayarat <pittayarat.sae@gmail.com>
2017-03-05 20:54:37 +0700

    wait for loading complete before show table

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -10,25 +10,32 @@ import {UserService} from "../service/user.service";
 export class HistoryComponent implements OnInit {
 
   allOrder= [];
+  allMenu=[]
+  isLoading = true;
 
   constructor(private orderService:OrderService, private userService: UserService) { 
 
   }
 
   ngOnInit() {
+    this.isLoading=true;
+    
     let userId = this.getUser().id;
     this.orderService.getOrderListByUserId(userId)
     .subscribe(
             data=> {
               this.allOrder = data;
               // console.log(data);
-
+            
+              this.isLoading=false;
+              
             },
             error => {
-               console.error("Cannnot get history!")
+               console.error("Cannnot get history order!")
             }
     )
   }
+
 
   private toDateString(utcDate){
     return new Date(utcDate).toDateString()+" "+new Date(utcDate).toTimeString().substring(0, 8)
