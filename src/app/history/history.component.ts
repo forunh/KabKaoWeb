e89614ca5ba1c5ca720ca2d3b4d7650c6758edcb
Unknown <moobinkung@windowslive.com>
2017-03-06 03:51:41 +0700

    - adding order history.

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../service/order.service';
 import {UserService} from "../service/user.service";
 import {Order} from '../model/order';
+import {Http, Response, URLSearchParams} from "@angular/http";
 
 @Component({
   selector: 'app-history',
@@ -12,24 +13,25 @@ export class HistoryComponent implements OnInit {
 
   allOrder:Array<Order> = [];
   isLoading = true;
+  private orderStatus = [];
 
-  constructor(private orderService:OrderService, private userService: UserService) { 
-
+  constructor(private orderService:OrderService, private userService: UserService, private http:Http) {
+    this.getHistory();
   }
 
   ngOnInit() {
     this.isLoading=true;
-    
+
     let userId = this.getUser().id;
     this.orderService.getOrderByUserId(userId)
     .subscribe(
             data=> {
               console.log(data);
-              
+
               this.allOrder = data;
-            
+
               this.isLoading=false;
-              
+
             },
             error => {
                console.error("Cannnot get history order!")
@@ -46,4 +48,15 @@ export class HistoryComponent implements OnInit {
       return this.userService.getMyUserData();
   }
 
+  private getHistory() {
+    let params:URLSearchParams = new URLSearchParams();
+    let user_id = this.userService.getMyUserData().id;
+    params.set("userid", String(user_id));
+    this.http.get('http://52.187.62.107:10500/kitchen/get_orderByUID', {search: params})
+      .map((response: Response) => response.json()).subscribe(
+      response => {
+        this.orderStatus = response;
+        this.orderService.getOrderListByUserId(user_id).subscribe((response) => { console.log(response);} )
+      });
+  }
 }
